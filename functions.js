cf0f74d470c0f7958350f7144a1b2fe97d4f8c4d
Almaha <mahaw999@gmail.com>
2023-05-05 02:25:31 +0300

    creating js file with check errors function

---
+++ functions.js
@@ -0,0 +1,57 @@
+let day = document.querySelector('#day-input')
+let day_title = document.querySelector('#day-title')
+let month = document.querySelector('#month-input')
+let month_title = document.querySelector('#month-title')
+let year = document.querySelector('#year-input')
+let year_title = document.querySelector('#year-title')
+let entered_years = document.querySelector('#entered-years')
+let entered_months = document.querySelector('#entered-months')
+let entered_days = document.querySelector('#entered-days')
+
+let error_nav = document.querySelector('#error_nav')
+error_nav.style.visibility = 'hidden'
+
+let btn = document.querySelector('#btn')
+
+let date = new Date()
+
+//check errors 
+btn.addEventListener('click', ()=>{
+    if(day.value === '' || /[a-zA-Z]/.test(day.value) || (day.value > 31 || day.value == 0)){
+    day.style.borderColor = 'red'
+    day_title.style.color = 'red'
+    error_nav.style.visibility = 'visible'
+    } else if(month.value === '' || /[a-zA-Z]/.test(month.value) || (month.value > 12 || month.value == 0)){
+        month.style.borderColor = 'red'
+        month_title.style.color = 'red'
+        error_nav.style.visibility = 'visible'
+    } else if(year.value === '' || /[a-zA-Z]/.test(year.value) || year.value > date.getFullYear()){
+    year.style.borderColor = 'red'
+    year_title.style.color = 'red'
+    error_nav.style.visibility = 'visible'
+    } 
+    else if((day.value > 30 && (month.value.match(/(4|6|9|11)/))) || (day.value > 28 && month.value == 2)){
+        month.style.borderColor = 'red'
+        month_title.style.color = 'red'
+        day.style.borderColor = 'red'
+        day_title.style.color = 'red'
+        error_nav.style.visibility = 'visible'
+        error_nav.innerHTML = 'date is invalid'
+    }
+})
+//change colors when writting something
+day.addEventListener('change', ()=>{
+    day_title.style.color = '#696969'
+    day.style.borderColor = 'grey'
+    error_nav.style.visibility = 'hidden'
+})
+month.addEventListener('change', ()=>{
+    month_title.style.color = '#696969'
+    month.style.borderColor = 'grey'
+    error_nav.style.visibility = 'hidden'
+})
+year.addEventListener('change', ()=>{
+    year_title.style.color = '#696969'
+    year.style.borderColor = 'grey'
+    error_nav.style.visibility = 'hidden'
+})
