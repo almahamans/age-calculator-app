357496f67c24db2245661cf82f18e4074f9feb2d
Almaha <mahaw999@gmail.com>
2023-05-08 00:48:03 +0300

    adding responsive style

--- functions.js
+++ functions.js
@@ -15,29 +15,39 @@ let btn = document.querySelector('#btn')
 
 let date = new Date()
 
-//check errors 
-btn.addEventListener('click', ()=>{
+//check errors
+function check(){
     if(day.value === '' || /[a-zA-Z]/.test(day.value) || (day.value > 31 || day.value == 0)){
-    day.style.borderColor = 'red'
-    day_title.style.color = 'red'
-    error_nav.style.visibility = 'visible'
-    } else if(month.value === '' || /[a-zA-Z]/.test(month.value) || (month.value > 12 || month.value == 0)){
-        month.style.borderColor = 'red'
-        month_title.style.color = 'red'
-        error_nav.style.visibility = 'visible'
-    } else if(year.value === '' || /[a-zA-Z]/.test(year.value) || year.value > date.getFullYear()){
-    year.style.borderColor = 'red'
-    year_title.style.color = 'red'
-    error_nav.style.visibility = 'visible'
-    } 
-    else if((day.value > 30 && (month.value.match(/(4|6|9|11)/))) || (day.value > 28 && month.value == 2)){
-        month.style.borderColor = 'red'
-        month_title.style.color = 'red'
         day.style.borderColor = 'red'
         day_title.style.color = 'red'
         error_nav.style.visibility = 'visible'
-        error_nav.innerHTML = 'date is invalid'
-    }
+        } else if(month.value === '' || /[a-zA-Z]/.test(month.value) || (month.value > 12 || month.value == 0)){
+            month.style.borderColor = 'red'
+            month_title.style.color = 'red'
+            error_nav.style.visibility = 'visible'
+        } else if(year.value === '' || /[a-zA-Z]/.test(year.value) || year.value > date.getFullYear()){
+        year.style.borderColor = 'red'
+        year_title.style.color = 'red'
+        error_nav.style.visibility = 'visible'
+        } else if((day.value > 30 && (month.value.match(/(4|6|9|11)/))) || (day.value > 28 && month.value == 2)){
+            month.style.borderColor = 'red'
+            month_title.style.color = 'red'
+            day.style.borderColor = 'red'
+            day_title.style.color = 'red'
+            error_nav.style.visibility = 'visible'
+            error_nav.innerHTML = 'date is invalid'
+        } 
+}
+//fire errors and/or calculate age 
+btn.addEventListener('click', ()=>{
+        if(day.value === '' || month.value === '' || year.value === ''){
+            check()
+        } else {
+            //calculate age and display on UI
+        entered_days.innerHTML= (date.getDay() - day.value + 30)
+        entered_months.innerHTML = (date.getMonth() - month.value + 12)
+        entered_years.innerHTML = (date.getFullYear() - year.value )
+        }
 })
 //change colors when writting something
 day.addEventListener('change', ()=>{
